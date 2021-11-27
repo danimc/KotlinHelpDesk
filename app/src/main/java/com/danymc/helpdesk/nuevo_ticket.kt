a15eeb90dde063c5c8d11c134ed11a1103b7c722
Daniel Mora <daniel_k310a@hotmail.com>
2021-11-27 14:28:22 -0600

    nuevas vistas

--- app/src/main/java/com/danymc/helpdesk/nuevo_ticket.kt
+++ app/src/main/java/com/danymc/helpdesk/nuevo_ticket.kt
@@ -1,37 +1,29 @@
 package com.danymc.helpdesk
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class nuevo_ticket: Fragment(){
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_nuevo_ticket, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button2).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_nuevo_ticket)
+      /**  view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
-
-        view.findViewById<Button>(R.id.button).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-
-
+      */
     }
 }
